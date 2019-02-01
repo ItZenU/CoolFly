60ff96eb8bfa402f212f5a19674606ad7ce41dd5
splexpe <46957762+splexpe@users.noreply.github.com>
2019-01-31 23:00:22 -0600

    implement new main, accidentally added old

--- src/CoolFly/Main.php
+++ src/CoolFly/Main.php
@@ -1,12 +1,32 @@
 <?php
+/**
+ *(C) Copyright 2019 Benzdoesdev
+ * Please dont sell this 
+ * (OR)
+ * claim this plugin as your own creation!
+ * Discord: "lil benzo#7065"
+ * Twitter: @UnendingApple
+ * My server IP: splexpe.mc-play.net
+ * My server PORT: 19132
+ */
+
+declare(strict_types=1);
+
 
 namespace Coolfly\Main;
 
+use pocketmine\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 
-class Main extends PluginBase{
+	class Main extends PluginBase implements Listener{
+	
+		const PREFIX = TextFormat::RED . "CoolFly" . TextFormat::WHITE . " > " . TextFormat::GOLD;
 	
 	public function __construct(Main $main){
 		parent::__construct($main, "fly", "Enable yourself or someone else to fly", ["fly"]);
@@ -18,31 +38,52 @@ class Main extends PluginBase{
 			return false;
 		}
 		if(!$sender->hasPermission("fly.use")){
-			$sender->sendMessage(self::NO_PERMISSION)
-			return false;
-		}
-		if(empty($args[0])){
-				if(!sender->isCreative()){
-					   $sender->sendMessage($sender->getAllowFlight() === false ? Main::PREFIX . TextFormat::RED . "You may no longer fly");
-					   $sender->setAllowFlight($sedner->getAllowFlight() === false ? true : false);
+				if(!$sender->isCreative()){
+					$sender->setAllowFlight(true);
+					$sedner->setFlying(true)
+					$sender->sendMessage(Main::PREFIX . TextFormat::GOLD . "You may now fly!");
+				}else{
+					$sender->setAllowFlight(false)
+					$sender->setFlying(false)
+					$sender->sendMessage(Main::PREFIX . TextFormat::RED . "Fly has been disabled!")
 					
 				}else{
 					   $sender->sendMessage(Main::PREFIX . TextFormat::RED . "You can only use this command in survival");
 					   return false;
 				}
-				return false;
+		}else{
+			$sender->sendMessage(Main::PREFIX . TextFormat::RED . "You haven't the permissions to use this command!");
+			return false;
 		}
 		if(API::getMainInstance()->getServer()->getPlayer($args[0]);
 				$player = API::getMainInstance()->getServer()->getPlayer($args[0]);
 				if($player->isCreative()){
-						$player->sendMessage($player->getAllowFlight() === false ? Main::PREFIX . TextFormat::GOLD . "You may now fly!")
-						$sedner->sendMessage($player->getAllowFlight() === Main::PREFIX . TextFormat::GOLD . "You allowed " . $player-getName() : Main:PREFIX . TextFormat . "You disabled fly for " . $player->getName());
+						$player->sendMessage($player->getAllowFlight() === false ? Main::PREFIX . TextFormat::GOLD . "You may now fly!");
+						$sedner->sendMessage($player->getAllowFlight() === Main::PREFIX . TextFormat::GOLD . "You enabled fly for " . $player-getName() : Main:PREFIX . TextFormat . "You disabled fly for " . $player->getName());
 						$player->setAllowFlight($player->getAllowFlight() === false ? true : false);
 				}else{
-						$sedner->sendMessage(Main::PREFIX . TextFormat::RED . $player->getName() . " is in creative mode");
+						$sender->sendMessage(Main::PREFIX . TextFormat::RED . $player->getName() . " is in creative mode");
 						return false;
 				}
 			}
-			return true;
+	    }
+	
+		public function onDamage(EntityDamageEvent $event) : void{
+			$entity = $event->getEntity();
+			$config = $this->getConfig();
+			if($config-getNested("onDamage_FlyReset") === true){
+				if(entity instanceof Player){
+					if($event instanceof EntityDamageByEntityEvent){
+						$damager = $event->getDamager();
+						if($damager instanceof Player){
+						if(!$damager->isCreative()){
+						$damager->getAllowFlight()){
+						$damager->sendMessage(Main::PREFIX . TextFormat::RED "Flight was disabled do to combat!")
+						$damager->setAllowFlight(false);
+						$damager->setFlying(false);
+					}
+				}
+			}
+		}
 	}
 }
